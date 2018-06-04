5b8a03018ee70864dbef398efb5759278bb280f2
Mortimer <mor582@csiro.au>
2018-06-04 12:13:55 +0800

    Correct Event time bug + add more flexible time out
    Signed-off-by: Mortimer <mor582@csiro.au>

--- lib/fluent/plugin/in_serialport.rb
+++ lib/fluent/plugin/in_serialport.rb
@@ -12,12 +12,16 @@ class SerialPortInput < Fluent::Plugin::Input
   config_param :tag, :string, default: "serial"
   config_param :eol, :string, default: $/
   config_param :include_time, :bool, default: false
+  config_param :timeout_message, :string, default: "Timeout"
+  config_param :time_out_in_seconds, :integer, default: 10
 
   def configure(conf)
     super
     @device = device
+    @timeout_message = timeout_message
+    @time_out_in_seconds = time_out_in_seconds
   end
-
+  
   def start
     super
     @serial = SerialPort.new(@com_port, @baud_rate, 8, 1, SerialPort::NONE)
@@ -35,10 +39,15 @@ class SerialPortInput < Fluent::Plugin::Input
         begin
           timenow = @include_time ? Time.now.to_s << ' ' : ''
           data = {@device => timenow << @serial.readline(@eol)}
-          router.emit(@tag, Engine.now, data)
+          router.emit(@tag, Fluent::EventTime.now, data)
+          timeout = 0
         rescue
-          $stderr.puts(caller) unless stopped?
-          break
+          timeout++
+          if timeout>time_out_in_seconds
+            timout = 0
+            $stderr.puts(caller) unless stopped?
+            router.emit(@tag, Fluent::EventTime.now, @timeout_message) unless stopped?
+          end
         end
       end
     end
